375f1ccb9df4ac29ebd675d34db4573e06a25553
hosseinmb7 <hosseinmb7@gmail.com>
2025-08-03 16:54:27 +0330

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
-const CACHE_NAME = "fish-box-cache-v1";
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/main.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("fish-box-tracker").then(function (cache) {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "main.js",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
   );
 });
